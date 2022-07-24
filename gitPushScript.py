2c8da8636c88beb55e7df221f25d0c8ec4903df2
Rajat16353 <guptarajat3199@gmail.com>
2022-07-24 14:04:39 +0530

    Modified: gitpushscript.py

--- gitPushScript.py
+++ gitPushScript.py
@@ -46,19 +46,21 @@ message = ''
 i = 0
 while i < len(status):
     if 'Changes not staged for commit:' in status[i]:
+        print("inside")
+        i += 3
         while i < len(status):
-            if status[i]:
+            if status[i] == '':
                 break
             message = re.sub(r"\s+", ' ', status[i][1:])
             commitCommand = 'git commit -m "'+message.capitalize()+'"'
 
-            p = subprocess.Popen("git add " + message[10:], stdout=subprocess.PIPE, shell=True)
+            p = subprocess.Popen('git add "' + message[10:] + '"', stdout=subprocess.PIPE, shell=True)
             p.wait()
-            # print(p.communicate())
+            print(p.communicate())
 
             p = subprocess.Popen(commitCommand, stdout=subprocess.PIPE, shell=True)
             p.wait()
-            # print(p.communicate(input=message)[0].decode('UTF-8'))
+            print(p.communicate(input=message)[0].decode('UTF-8'))
             i += 1
     elif 'Untracked files:' in status[i]:
         for stat in status[i+2:len(status)-3]:
@@ -66,7 +68,7 @@ while i < len(status):
             message = 'Add solutions for'+ file
             commitCommand = 'git commit -m "'+message+'"'
 
-            p = subprocess.Popen("git add " + file, stdout=subprocess.PIPE, shell=True)
+            p = subprocess.Popen('git add "' + file[1:] + '"', stdout=subprocess.PIPE, shell=True)
             p.wait()
             # print(p.communicate())
 
@@ -76,5 +78,5 @@ while i < len(status):
         break
     i += 1
 
-        # p = subprocess.Popen("git push", stdout=subprocess.PIPE, shell=True)
-        # print(p.communicate())
+p = subprocess.Popen("git push", stdout=subprocess.PIPE, shell=True)
+print(p.communicate())
