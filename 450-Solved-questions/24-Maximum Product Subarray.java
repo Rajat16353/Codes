46523d39ae274aa84ed29cbff4b32bcbf2fce98f
Rajat16353 <guptarajat3199@gmail.com>
2022-07-27 20:51:44 +0530

    Add solutions for 450-Solved-questions/24-Maximum Product Subarray.java

---
+++ 450-Solved-questions/24-Maximum Product Subarray.java
@@ -0,0 +1,71 @@
+// Given an array Arr[] that contains N integers (may be positive, negative or zero). Find the product of the maximum product subarray.
+
+// Example 1:
+
+// Input:
+// N = 5
+// Arr[] = {6, -3, -10, 0, 2}
+// Output: 180
+// Explanation: Subarray with maximum product
+// is [6, -3, -10] which gives product as 180.
+// Example 2:
+
+// Input:
+// N = 6
+// Arr[] = {2, 3, 4, 5, -1, 0}
+// Output: 120
+// Explanation: Subarray with maximum product
+// is [2, 3, 4, 5] which gives product as 120.
+// Your Task:
+// You don't need to read input or print anything. Your task is to complete the function maxProduct() which takes the array of integers arr and n as parameters and returns an integer denoting the answer.
+// Note: Use 64-bit integer data type to avoid overflow.
+
+// Expected Time Complexity: O(N)
+// Expected Auxiliary Space: O(1)
+
+// Constraints:
+// 1 ≤ N ≤ 500
+// -102 ≤ Arri ≤ 102
+
+// { Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+  public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine());
+        while (tc-- > 0) {
+            int n = Integer.parseInt(br.readLine());
+            int[] arr = new int[n];
+            String[] inputLine = br.readLine().split(" ");
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+
+            System.out.println(new Solution().maxProduct(arr, n));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution {
+    // Function to find maximum product subarray
+    long maxProduct(int[] arr, int n) {
+        long maxP = arr[0];
+        long minP = arr[0];
+        long ans = arr[0];
+        long choice1, choice2;
+        for(int i = 1; i < n; i++) {
+            choice1 = arr[i] * maxP;
+            choice2 = arr[i] * minP;
+            maxP = Math.max(arr[i], Math.max(choice1, choice2));
+            minP = Math.min(arr[i], Math.min(choice1, choice2));
+            ans = Math.max(ans, maxP);
+        }
+        return ans;
+    }
+}
