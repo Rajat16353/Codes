cb3c34fd07fe05bd03636d74cd08ad3786234e98
Rajat16353 <guptarajat3199@gmail.com>
2022-07-27 22:15:01 +0530

    Modified: 450questionsscript.py

--- 450QuestionsScript.py
+++ 450QuestionsScript.py
@@ -1,27 +1,37 @@
-path_to_read_file = "C:/Users/gupta/Documents/450 problems.txt"
+import subprocess
+import re
+
+# This is our shell command, executed by Popen.
+p = subprocess.Popen("dir", stdout=subprocess.PIPE, shell=True, cwd='./450-Solved-questions')
+
+lines = p.communicate()[0].decode('UTF-8').split('\r\n')
+
+files = {}
+
+for line in lines[7:-4]:
+    line = re.sub(r"\s+", ' ',line)
+    index, name = line.split(' ', maxsplit=3)[3].split('-')
+    files[int(index)] = name
+
+print(len(files))
+
 path_to_write_file = "450-Solved-questions/README.md"
-with open(path_to_read_file, 'r') as f:
-    lines = f.readlines();
-    # print(line)
-data_to_write = "##450 Problems sheet by Love Babar DSA sheet"+"\n"+"\n"
+
+
+data_to_write = "# 450 Problems sheet by Love Babar DSA sheet"+"\n"+"\n"
 data_to_write = data_to_write + "| Sr. No | Name of the problem | Sr. No | Name of the problem |\n"
 data_to_write = data_to_write + "| ------ | ------------------- | ------ | ------------------- |\n"
 
-for i in range(len(lines)):
-    lines[i] = lines[i].replace('.', '')
-    lines[i] = lines[i].replace('\n', '')
 
-for line in lines:
-    index = line.split(' ', maxsplit=1)[0]
-    name = line.split(' ', maxsplit=1)[1]
-    link = '450-Solved-questions/'+index+'-'+name
-    if int(index) % 2 == 1:
-        data_to_write += '| '+index+' | ['+name+'](https://github.com/Rajat16353/Codes/blob/master/' + link + ') '
+for i in range(1, len(files)+1):
+    name = files[i].split('.')[0]
+    link = '450-Solved-questions/'+str(i)+'-'+files[i].replace(' ', '%20')
+    if i % 2 == 1:
+        data_to_write = data_to_write + '| '+str(i)+' | ['+name+'](https://github.com/Rajat16353/Codes/blob/master/' + link + ') '
     else:
-        data_to_write += '| '+index+' | ['+name+'](https://github.com/Rajat16353/Codes/blob/master/' + link + ') |\n'
+        data_to_write = data_to_write + '| '+str(i)+' | ['+name+'](https://github.com/Rajat16353/Codes/blob/master/' + link + ') |\n'
 
 # print(data_to_write)
-f.close()
 
 with open(path_to_write_file, 'w') as f:
     f.write(data_to_write)
