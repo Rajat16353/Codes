e8fc4f9f0af8551b01202f2580278a6f3efbf3f0
Rajat16353 <guptarajat3199@gmail.com>
2023-09-12 15:01:53 +0530

    Add solutions for Leet-Code/151-200/169-Minimum Deletions to Make Character Frequencies Unique.java

---
+++ Leet-Code/151-200/169-Minimum Deletions to Make Character Frequencies Unique.java
@@ -0,0 +1,67 @@
+// A string s is called good if there are no two different characters in s that have the same frequency.
+
+// Given a string s, return the minimum number of characters you need to delete to make s good.
+
+// The frequency of a character in a string is the number of times it appears in the string. For example, in the string "aab", the frequency of 'a' is 2, while the frequency of 'b' is 1.
+
+ 
+
+// Example 1:
+
+// Input: s = "aab"
+// Output: 0
+// Explanation: s is already good.
+// Example 2:
+
+// Input: s = "aaabbbcc"
+// Output: 2
+// Explanation: You can delete two 'b's resulting in the good string "aaabcc".
+// Another way it to delete one 'b' and one 'c' resulting in the good string "aaabbc".
+// Example 3:
+
+// Input: s = "ceabaacb"
+// Output: 2
+// Explanation: You can delete both 'c's resulting in the good string "eabaab".
+// Note that we only care about characters that are still in the string at the end (i.e. frequency of 0 is ignored).
+ 
+
+// Constraints:
+
+// 1 <= s.length <= 105
+// s contains only lowercase English letters.
+
+// Using HashMap and HashSet
+class Solution {
+    public int minDeletions(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+        Set<Integer> set = new HashSet<>();
+
+        for (char c: s.toCharArray()) {
+            if (map.containsKey(c)) {
+                map.put(c, map.get(c) + 1);
+            } else {
+                map.put(c, 1);
+            }
+        }
+        int deleteChars = 0;
+        for (Map.Entry<Character, Integer> entry : map.entrySet()) {
+            int val = entry.getValue();
+            if (set.contains(val)) {
+                val--;
+                while (val != 0) {
+                    if (set.contains(val)) {
+                        val--;
+                        continue;
+                    }
+                    set.add(val);
+                    break;
+                }
+                deleteChars += (entry.getValue() - val);
+            } else {
+                set.add(val);
+            }
+        }
+
+        return deleteChars;
+    }
+}
