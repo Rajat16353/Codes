8c993b508734dc52a8f920a39384a7e9c65066db
Rajat16353 <guptarajat3199@gmail.com>
2023-10-01 09:06:05 +0530

    Modified: leet-code/151-200/188-reverse words in a string iii.java

--- Leet-Code/151-200/188-Reverse Words in a String III.java
+++ Leet-Code/151-200/188-Reverse Words in a String III.java
@@ -45,3 +45,17 @@ class Solution {
         return new String(s_arr);
     }
 }
+
+// Using in-build methods
+public class Solution {
+    public String reverseWords(String s) {
+        String[] words = s.split(" ");
+        StringBuilder result = new StringBuilder();
+        
+        for (String word : words) {
+            result.append(new StringBuilder(word).reverse()).append(" ");
+        }
+        
+        return result.toString().trim();
+    }
+}
