a8af249c2584170272c3019d32bc24eafd22d413
Rajat16353 <guptarajat3199@gmail.com>
2024-01-20 12:27:39 +0530

    Modified: leetcodescript.py

--- leetCodeScript.py
+++ leetCodeScript.py
@@ -17,13 +17,13 @@ for folder in os.listdir(directory):
 
 path_to_write_file = "Leet-Code/README.md"
 data_to_write = "# Codes\n\n## Solutions for the problems available on leetcode.\n\n"
-data_to_write = data_to_write + "| Sr. No | Name of the problem | Sr. No | Name of the problem |\n"
-data_to_write = data_to_write + "| ------ | ------------------- | ------ | ------------------- |\n"
+data_to_write = data_to_write + "| Sr. No | Name of the problem | Sr. No | Name of the problem | Sr. No | Name of the problem |\n"
+data_to_write = data_to_write + "| ------ | ------------------- | ------ | ------------------- | ------ | ------------------- |\n"
 
 for i in range(1, len(files)+1):
     name = files[i]['filename']
     link = files[i]['link']
-    if i % 2 == 1:
+    if i % 3 != 0:
         data_to_write = data_to_write + '| '+str(i)+' | ['+name+'](https://github.com/Rajat16353/Codes/blob/master/' + link + ') '
     else:
         data_to_write = data_to_write + '| '+str(i)+' | ['+name+'](https://github.com/Rajat16353/Codes/blob/master/' + link + ') |\n'
