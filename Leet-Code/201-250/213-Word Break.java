b303ad42d7c795a779b0867f3bf5d53f9fd80158
Rajat16353 <guptarajat3199@gmail.com>
2024-05-05 23:41:35 +0530

    Add solutions for Leet-Code/201-250/213-Word Break.java

---
+++ Leet-Code/201-250/213-Word Break.java
@@ -0,0 +1,61 @@
+// Given a string s and a dictionary of strings wordDict, return true if s can be segmented into a space-separated sequence of one or more dictionary words.
+
+// Note that the same word in the dictionary may be reused multiple times in the segmentation.
+
+ 
+
+// Example 1:
+
+// Input: s = "leetcode", wordDict = ["leet","code"]
+// Output: true
+// Explanation: Return true because "leetcode" can be segmented as "leet code".
+// Example 2:
+
+// Input: s = "applepenapple", wordDict = ["apple","pen"]
+// Output: true
+// Explanation: Return true because "applepenapple" can be segmented as "apple pen apple".
+// Note that you are allowed to reuse a dictionary word.
+// Example 3:
+
+// Input: s = "catsandog", wordDict = ["cats","dog","sand","and","cat"]
+// Output: false
+ 
+
+// Constraints:
+
+// 1 <= s.length <= 300
+// 1 <= wordDict.length <= 1000
+// 1 <= wordDict[i].length <= 20
+// s and wordDict[i] consist of only lowercase English letters.
+// All the strings of wordDict are unique.
+
+// DP using for loop inside dfs
+class Solution {
+    Set<String> dict;
+    Map<String, Boolean> memo;
+
+    private Boolean dfs(String word) {
+        if (memo.containsKey(word)) {
+            return memo.get(word);
+        }
+        if (dict.contains(word)) {
+            return true;
+        }
+        for (int i = 1; i < word.length(); i++) {
+            String subString = word.substring(0, i);
+            if (dict.contains(subString) && dfs(word.substring(i))) {
+                memo.put(word, true);
+                return true;
+            }
+        }
+        memo.put(word, false);
+        return false;
+    }
+
+    public boolean wordBreak(String s, List<String> wordDict) {
+        dict = new HashSet<>(wordDict);
+        memo = new HashMap<>();
+        
+        return dfs(s);
+    }
+}
