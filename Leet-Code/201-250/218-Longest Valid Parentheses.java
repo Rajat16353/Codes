331dbbfe22ccdf2fb49ac74ba23bde499cae3d9b
Rajat16353 <guptarajat3199@gmail.com>
2024-05-18 11:54:10 +0530

    Add solutions for Leet-Code/201-250/218-Longest Valid Parentheses.java

---
+++ Leet-Code/201-250/218-Longest Valid Parentheses.java
@@ -0,0 +1,47 @@
+// Given a string containing just the characters '(' and ')', return the length of the longest valid (well-formed) parentheses 
+// substring
+// .
+
+ 
+
+// Example 1:
+
+// Input: s = "(()"
+// Output: 2
+// Explanation: The longest valid parentheses substring is "()".
+// Example 2:
+
+// Input: s = ")()())"
+// Output: 4
+// Explanation: The longest valid parentheses substring is "()()".
+// Example 3:
+
+// Input: s = ""
+// Output: 0
+ 
+
+// Constraints:
+
+// 0 <= s.length <= 3 * 104
+// s[i] is '(', or ')'.
+
+class Solution {
+    public int longestValidParentheses(String s) {
+        int maxValid = 0;
+        Stack<Integer> stack = new Stack<>();
+        stack.push(-1);
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (ch == '(') {
+                stack.push(i);
+            } else {
+                stack.pop();
+                if (stack.isEmpty()) {
+                    stack.push(i);
+                }
+                maxValid = Math.max(maxValid, i - stack.peek());
+            }
+        }
+        return maxValid;
+    }
+}
