5ecd0eb360f5e04950fff645c4d104404641b0de
Rajat16353 <guptarajat3199@gmail.com>
2024-08-15 13:31:41 +0530

    Modified: leet-code/101-150/135-valid palindrome.java

--- Leet-Code/101-150/135-Valid Palindrome.java
+++ Leet-Code/101-150/135-Valid Palindrome.java
@@ -44,3 +44,22 @@ class Solution {
         return isPalindrome;
     }
 }
+
+// Using Recursion
+class Solution {
+    private Boolean isPalindrome(int i, String s) {
+        if (i >= s.length()/2) return true;
+
+        if (s.charAt(i) != s.charAt(s.length() - i - 1)) return false;
+
+        return isPalindrome(i + 1, s);
+    }
+
+    public boolean isPalindrome(String s) {
+        s = s.toLowerCase();
+        s = s.replaceAll("[^a-zA-Z0-9]", "");
+
+        if (s.length() < 1) return true;
+        return isPalindrome(0, s);
+    }
+}
