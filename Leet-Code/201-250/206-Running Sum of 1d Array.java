0337c8b40cf12a6a5553c1925dcc5fa799e87e0e
Rajat <guptarajat3199@gmail.com>
2024-11-17 19:47:31 +0530

    Modified: leet-code/201-250/206-running sum of 1d array.java

--- Leet-Code/201-250/206-Running Sum of 1d Array.java
+++ Leet-Code/201-250/206-Running Sum of 1d Array.java
@@ -37,3 +37,14 @@ class Solution {
         return runningSum;
     }
 }
+
+// Without extra space
+class Solution {
+    public int[] runningSum(int[] nums) {
+        for (int i = 1; i < nums.length; i++) {
+            nums[i] = nums[i] + nums[i - 1];
+        }
+
+        return nums;
+    }
+}
