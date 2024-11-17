b09d3db6cf7ba1ec4b22c495a78e224b82d45d77
Rajat <guptarajat3199@gmail.com>
2024-11-17 15:13:57 +0530

    Modified: leet-code/51-100/67-next permutation.java

--- Leet-Code/51-100/67-Next Permutation.java
+++ Leet-Code/51-100/67-Next Permutation.java
@@ -68,3 +68,30 @@ class Solution {
         
     }
 }
+
+// Time Complexity O(3n)
+class Solution {
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+    private void reverse(int [] nums, int start, int last) {
+        while(start < last) swap(nums, start++, last--);
+    }
+
+    public void nextPermutation(int[] nums) {
+        if (nums == null || nums.length == 1) return;
+        
+        int i = nums.length - 2;
+        while(i >= 0 && nums[i] >= nums[i + 1]) i--;
+        if (i >= 0) {
+            int j = nums.length - 1;
+            while(nums[j] <= nums[i]) j--;
+            swap(nums, i, j);
+        }
+
+        reverse(nums, i + 1, nums.length - 1);
+    }
+}
