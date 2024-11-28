675224d72dee90061e4644223ad50fd8575117a1
Rajat <guptarajat3199@gmail.com>
2024-11-28 23:45:24 +0530

    Add solutions for Leet-Code/201-250/246-Binary Tree Zigzag Level Order Traversal.java

---
+++ Leet-Code/201-250/246-Binary Tree Zigzag Level Order Traversal.java
@@ -0,0 +1,73 @@
+// Given the root of a binary tree, return the zigzag level order traversal of its nodes' values. (i.e., from left to right, then right to left for the next level and alternate between).
+
+ 
+
+// Example 1:
+
+
+// Input: root = [3,9,20,null,null,15,7]
+// Output: [[3],[20,9],[15,7]]
+// Example 2:
+
+// Input: root = [1]
+// Output: [[1]]
+// Example 3:
+
+// Input: root = []
+// Output: []
+ 
+
+// Constraints:
+
+// The number of nodes in the tree is in the range [0, 2000].
+// -100 <= Node.val <= 100
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        Boolean reverse = false;
+        queue.offer(root);
+
+        while(!queue.isEmpty()) {
+            int size = queue.size();
+            List<Integer> row = new ArrayList<>();
+            while(size > 0) {
+                TreeNode node = queue.poll();
+                if (!reverse) {
+                    row.add(node.val);
+                } else {
+                    row.add(0, node.val);
+                }
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }   
+                size--;
+            }
+            reverse = !reverse;
+            result.add(row);
+        }
+        return result;
+    }
+}
