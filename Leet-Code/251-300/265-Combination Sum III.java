54f8b9d049c99eb5f9eaa49e841285e127c8c50c
Rajat <guptarajat3199@gmail.com>
2024-12-05 00:37:00 +0530

    Add solutions for Leet-Code/251-300/265-Combination Sum III.java

---
+++ Leet-Code/251-300/265-Combination Sum III.java
@@ -0,0 +1,60 @@
+// Find all valid combinations of k numbers that sum up to n such that the following conditions are true:
+
+// Only numbers 1 through 9 are used.
+// Each number is used at most once.
+// Return a list of all possible valid combinations. The list must not contain the same combination twice, and the combinations may be returned in any order.
+
+ 
+
+// Example 1:
+
+// Input: k = 3, n = 7
+// Output: [[1,2,4]]
+// Explanation:
+// 1 + 2 + 4 = 7
+// There are no other valid combinations.
+// Example 2:
+
+// Input: k = 3, n = 9
+// Output: [[1,2,6],[1,3,5],[2,3,4]]
+// Explanation:
+// 1 + 2 + 6 = 9
+// 1 + 3 + 5 = 9
+// 2 + 3 + 4 = 9
+// There are no other valid combinations.
+// Example 3:
+
+// Input: k = 4, n = 1
+// Output: []
+// Explanation: There are no valid combinations.
+// Using 4 different numbers in the range [1,9], the smallest sum we can get is 1+2+3+4 = 10 and since 10 > 1, there are no valid combination.
+ 
+
+// Constraints:
+
+// 2 <= k <= 9
+// 1 <= n <= 60
+
+class Solution {
+    private void combinationSum3(int k, int target, int curr, List<Integer> combination, List<List<Integer>> result) {
+        if (target == 0 && combination.size() == k) {
+            result.add(new ArrayList<>(combination));
+            return;
+        }
+        
+        for(int i = curr; i <= 9; i++) {
+            if (combination.size() >= k) break;
+
+            combination.add(i);
+            combinationSum3(k, target - i, i + 1, combination, result);
+            combination.remove(combination.size() - 1);
+        }
+    }
+
+    public List<List<Integer>> combinationSum3(int k, int n) {
+        List<List<Integer>> result = new ArrayList<>();
+
+        combinationSum3(k, n, 1, new ArrayList<>(), result);
+        return result;
+    }
+}
