c645c7415ad80db860490d8bb6808913faaaad44
Rajat <guptarajat3199@gmail.com>
2024-12-06 00:07:33 +0530

    Modified: 450-solved-questions/1-50/9-find duplicate in an array of n+1 integers.java

--- 450-Solved-questions/1-50/9-Find duplicate in an array of N+1 Integers.java
+++ 450-Solved-questions/1-50/9-Find duplicate in an array of N+1 Integers.java
@@ -49,3 +49,17 @@ class Solution {
         }
     }
 }
+
+class Solution {
+    public int findDuplicate(int[] nums) {
+        for(int i = 0; i < nums.length; i++) {
+            int index = Math.abs(nums[i]);
+            if (nums[index - 1] < 0) {
+                return index;
+            } else {
+                nums[index - 1] *= -1;
+            }
+        }
+        return 0;
+    }
+}
