53de171364f1cecd87f7e6fb555115e0b57f79b2
Rajat <guptarajat3199@gmail.com>
2024-12-12 23:53:57 +0530

    Modified: leet-code/1-50/3-longest substring without repeating characters.java

--- Leet-Code/1-50/3-Longest Substring without Repeating Characters.java
+++ Leet-Code/1-50/3-Longest Substring without Repeating Characters.java
@@ -33,31 +33,45 @@ s consists of English letters, digits, symbols and spaces.
 
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        int i=0,j=0,k,max=0;
-        String a = "";
-        while(i != s.length())
-        {
-            for(k=0;k<j;k++)
-            {
-                if(a.charAt(k)==s.charAt(i))
-                    break;
+        Map<Character, Integer> map = new HashMap<>();
+        int maxLen = 0;
+        int l = 0, r = 0;
+
+        while (r < s.length()) {
+            char rc = s.charAt(r);
+            map.put(rc, map.getOrDefault(rc, 0) + 1);
+
+            while (map.get(rc) > 1) {
+                char lc = s.charAt(l);
+                map.put(lc, map.get(lc) - 1);
+                l += 1;
             }
-            if(k==j)
-            {
-                a = a+s.charAt(i);
-                j++;
-            }
-            else
-            {
-                if(max < a.length())
-                    max = a.length();
-                a = a.substring(k+1)+s.charAt(i); 
-                j = a.length();
-            }
-            i++;
+            maxLen = Math.max(maxLen, r - l + 1);
+            r += 1;
         }
-        if(max < a.length())
-            max = a.length();
-        return max;
+
+        return maxLen;
+    }
+}
+
+// Optimised approach
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        Map<Character, Integer> charIndex = new HashMap<>();
+        int maxLen = 0;
+        int l = 0, r = 0;
+
+        while (r < s.length()) {
+            char rc = s.charAt(r);
+            if (charIndex.containsKey(rc) && charIndex.get(rc) >= l) {
+                l = charIndex.get(rc) + 1;
+            }
+            charIndex.put(rc, r);
+
+            maxLen = Math.max(maxLen, r - l + 1);
+            r += 1;
+        }
+
+        return maxLen;
     }
 }
