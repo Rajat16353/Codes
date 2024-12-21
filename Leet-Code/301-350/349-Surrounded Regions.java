cfe070657577724955cfa92bd04e9a259632ab2c
Rajat16353 <guptarajat3199@gmail.com>
2024-12-22 02:00:46 +0530

    Add solutions for Leet-Code/301-350/349-Surrounded Regions.java

---
+++ Leet-Code/301-350/349-Surrounded Regions.java
@@ -0,0 +1,77 @@
+// You are given an m x n matrix board containing letters 'X' and 'O', capture regions that are surrounded:
+
+// Connect: A cell is connected to adjacent cells horizontally or vertically.
+// Region: To form a region connect every 'O' cell.
+// Surround: The region is surrounded with 'X' cells if you can connect the region with 'X' cells and none of the region cells are on the edge of the board.
+// A surrounded region is captured by replacing all 'O's with 'X's in the input matrix board.
+
+ 
+
+// Example 1:
+
+// Input: board = [["X","X","X","X"],["X","O","O","X"],["X","X","O","X"],["X","O","X","X"]]
+
+// Output: [["X","X","X","X"],["X","X","X","X"],["X","X","X","X"],["X","O","X","X"]]
+
+// Explanation:
+
+
+// In the above diagram, the bottom region is not captured because it is on the edge of the board and cannot be surrounded.
+
+// Example 2:
+
+// Input: board = [["X"]]
+
+// Output: [["X"]]
+
+ 
+
+// Constraints:
+
+// m == board.length
+// n == board[i].length
+// 1 <= m, n <= 200
+// board[i][j] is 'X' or 'O'.
+
+class Solution {
+    public void solve(char[][] board) {
+        int m = board.length;
+        int n = board[0].length;
+        for (int r = 0; r < m; r++) {
+            for (int c = 0; c < n; c++) {
+                if ((r == 0 || c == 0 || r == m - 1 || c == n - 1) && board[r][c] == 'O') {
+                    dfs(board, r, c, 'S');
+                }
+            }
+        }
+
+        for (int r = 0; r < m; r++) {
+            for (int c = 0; c < n; c++) {
+                if (board[r][c] == 'O') {
+                    dfs(board, r, c, 'X');
+                }
+            }
+        }
+
+        for (int r = 0; r < m; r++) {
+            for (int c = 0; c < n; c++) {
+                if (board[r][c] == 'S') {
+                    board[r][c] = 'O';
+                }
+            }
+        }
+    }
+
+    private final int[][] dirs = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+
+    private void dfs(char[][] board, int r, int c, char ch) {
+        if (r < 0 || c < 0 || r >= board.length || c >= board[0].length || board[r][c] == 'X' || board[r][c] == 'S') {
+            return;
+        }
+        board[r][c] = ch;
+
+        for (int[] d: dirs) {
+            dfs(board, r + d[0], c + d[1], ch);
+        }
+    }
+}
