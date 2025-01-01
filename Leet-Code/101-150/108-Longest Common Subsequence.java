3c0f54f9430bf49a6f3a2c5a2080f5edc213c3d1
Rajat16353 <guptarajat3199@gmail.com>
2025-01-02 00:46:48 +0530

    Modified: leet-code/101-150/108-longest common subsequence.java

--- Leet-Code/101-150/108-Longest Common Subsequence.java
+++ Leet-Code/101-150/108-Longest Common Subsequence.java
@@ -57,3 +57,78 @@ class Solution {
         return len[len1-1][len2-1];
     }
 }
+
+// Using recursion and dp
+class Solution {
+    public int longestCommonSubsequence(String text1, String text2) {
+        int l1 = text1.length();
+        int l2 = text2.length();
+        int[][] dp = new int[l1][l2];
+        Arrays.stream(dp).forEach(row -> Arrays.fill(row, -1));
+        return recursion(l1 - 1, l2 - 1, text1, text2, dp);
+    }
+
+    private int recursion(int idx1, int idx2, String text1, String text2, int[][] dp) {
+        if (idx1 < 0 || idx2 < 0) return 0;
+
+        if (dp[idx1][idx2] != -1) return dp[idx1][idx2];
+        
+        if (text1.charAt(idx1) == text2.charAt(idx2)) {
+            return dp[idx1][idx2] = 1 + recursion(idx1 - 1, idx2 - 1, text1, text2, dp);
+        }
+
+        int firstString = recursion(idx1 - 1, idx2, text1, text2, dp);
+        int secondString = recursion(idx1, idx2 - 1, text1, text2, dp);
+
+        return dp[idx1][idx2] = Math.max(firstString, secondString);
+    }
+}
+
+// Using tabulation
+class Solution {
+    public int longestCommonSubsequence(String text1, String text2) {
+        int l1 = text1.length();
+        int l2 = text2.length();
+        int[][] dp = new int[l1 + 1][l2 + 1];
+
+        for (int i = 0; i <= l1; i++) dp[i][0] = 0;
+        for (int i = 0; i <= l2; i++) dp[0][i] = 0;
+
+        for (int idx1 = 1; idx1 <= l1; idx1++) {
+            for (int idx2 = 1; idx2 <= l2; idx2++) {
+                if (text1.charAt(idx1 - 1) == text2.charAt(idx2 - 1)) {
+                    dp[idx1][idx2] = 1 + dp[idx1 - 1][idx2 - 1];
+                    continue;
+                }
+
+                dp[idx1][idx2] = Math.max(dp[idx1 - 1][idx2], dp[idx1][idx2 - 1]);
+            }
+        }
+        
+        return dp[l1][l2];
+    }
+}
+
+// Using memory optimised tabulation
+class Solution {
+    public int longestCommonSubsequence(String text1, String text2) {
+        int l1 = text1.length();
+        int l2 = text2.length();
+        int[] dp = new int[l2 + 1];
+
+        for (int idx1 = 1; idx1 <= l1; idx1++) {
+            int[] cur = new int[l2 + 1];
+            for (int idx2 = 1; idx2 <= l2; idx2++) {
+                if (text1.charAt(idx1 - 1) == text2.charAt(idx2 - 1)) {
+                    cur[idx2] = 1 + dp[idx2 - 1];
+                    continue;
+                }
+
+                cur[idx2] = Math.max(dp[idx2], cur[idx2 - 1]);
+            }
+            dp = cur;
+        }
+        
+        return dp[l2];
+    }
+}
