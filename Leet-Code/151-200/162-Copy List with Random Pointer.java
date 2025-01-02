7823229601a96b67ffbe4cabd4ae3dc2db687d87
Rajat16353 <guptarajat3199@gmail.com>
2025-01-02 20:57:11 +0530

    Modified: leet-code/151-200/162-copy list with random pointer.java

--- Leet-Code/151-200/162-Copy List with Random Pointer.java
+++ Leet-Code/151-200/162-Copy List with Random Pointer.java
@@ -53,6 +53,7 @@ class Node {
 }
 */
 
+// without extra memory
 class Solution {
     public Node copyRandomList(Node head) {
         HashMap<Node, Node> nodeMap = new HashMap<>();
@@ -77,5 +78,53 @@ class Solution {
         }
 
         return nodeMap.get(head);
+    }
+}
+
+// without extra memory
+/*
+// Definition for a Node.
+class Node {
+    int val;
+    Node next;
+    Node random;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+}
+*/
+
+class Solution {
+    public Node copyRandomList(Node head) {
+        Node temp = head;
+        while (temp != null) {
+            Node dummy = new Node(temp.val);
+            dummy.next = temp.next;
+            temp.next = dummy;
+            temp = dummy.next;
+        }
+
+        temp = head;
+        while(temp != null && temp.next != null) {
+            Node copyNode = temp.next;
+            if (temp.random != null) copyNode.random = temp.random.next;
+            temp = temp.next.next;
+        }
+
+        Node dummy = new Node(-1);
+        Node res = dummy;
+        temp = head;
+        while(temp != null && temp.next != null) {
+            Node copyNode = temp.next;
+            dummy.next = copyNode;
+            temp.next = temp.next.next;
+            dummy = dummy.next;
+            temp = temp.next;
+        }
+
+        return res.next;
     }
 }
