ebc9b025b68417b5459529876d3c0bbfd4281e1d
Rajat16353 <guptarajat3199@gmail.com>
2025-01-06 23:46:34 +0530

    Modified: leet-code/151-200/177-longest string chain.java

--- Leet-Code/151-200/177-Longest String Chain.java
+++ Leet-Code/151-200/177-Longest String Chain.java
@@ -61,3 +61,46 @@ class Solution {
         return longestWordChain;
     }
 }
+
+// Using tabulation
+class Solution {
+    public int longestStrChain(String[] words) {
+        int n = words.length;
+        int ans = -1;
+        Arrays.sort(words, (a, b) -> Integer.compare(a.length(), b.length()));
+
+        int[] dp = new int[n];
+        Arrays.fill(dp, 1);
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (checkPossible(words[i], words[j]) && 1 + dp[j] > dp[i]) {
+                    dp[i] = 1 + dp[j];
+                }
+            }
+            if (ans < dp[i]) {
+                ans = dp[i];
+            }
+        }
+
+        return ans;
+    }    
+
+    private boolean checkPossible(String s1, String s2) {
+        if (s1.length() != s2.length() + 1) return false;
+        int first = 0;
+        int second = 0;
+
+        while (first < s1.length()) {
+            if (second < s2.length() && s1.charAt(first) == s2.charAt(second)) {
+                first += 1;
+                second += 1;
+            } else {
+                first += 1;
+            }
+        }
+
+        if (first == s1.length() && second == s2.length()) return true;
+        return false;
+    }
+}
