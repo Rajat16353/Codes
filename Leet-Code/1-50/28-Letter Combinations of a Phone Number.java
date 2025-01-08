116b727c9a0257917f32886522182618f5b6b9b9
Rajat16353 <guptarajat3199@gmail.com>
2025-01-08 22:39:51 +0530

    Modified: leet-code/1-50/28-letter combinations of a phone number.java

--- Leet-Code/1-50/28-Letter Combinations of a Phone Number.java
+++ Leet-Code/1-50/28-Letter Combinations of a Phone Number.java
@@ -1,46 +1,50 @@
-# Given a string containing digits from 2-9 inclusive, return all possible letter combinations that the number could represent. Return the answer in any order.
+// # Given a string containing digits from 2-9 inclusive, return all possible letter combinations that the number could represent. Return the answer in any order.
 
-# A mapping of digit to letters (just like on the telephone buttons) is given below. Note that 1 does not map to any letters.
+// # A mapping of digit to letters (just like on the telephone buttons) is given below. Note that 1 does not map to any letters.
 
 
 
  
 
-# Example 1:
+// # Example 1:
 
-# Input: digits = "23"
-# Output: ["ad","ae","af","bd","be","bf","cd","ce","cf"]
-# Example 2:
+// # Input: digits = "23"
+// # Output: ["ad","ae","af","bd","be","bf","cd","ce","cf"]
+// # Example 2:
 
-# Input: digits = ""
-# Output: []
-# Example 3:
+// # Input: digits = ""
+// # Output: []
+// # Example 3:
 
-# Input: digits = "2"
-# Output: ["a","b","c"]
+// # Input: digits = "2"
+// # Output: ["a","b","c"]
  
 
-# Constraints:
+// # Constraints:
 
-# 0 <= digits.length <= 4
-# digits[i] is a digit in the range ['2', '9'].
+// # 0 <= digits.length <= 4
+// # digits[i] is a digit in the range ['2', '9'].
 
-# Solved using backtracking
+// # Solved using backtracking
 class Solution {
     Map<Character, Character[]> map;
     List<String> result;
-    private void backtrack(String digits, String word, int index) {
+    private void backtrack(String digits, StringBuilder word, int index) {
         if (word.length() == digits.length()) {
-            result.add(word);
+            result.add(word.toString());
             return;
         }
         for(char c: map.get(digits.charAt(index))) {
-            backtrack(digits, word + c, index + 1);
+            word.append(c);
+            backtrack(digits, word, index + 1);
+            word.setLength(word.length() - 1);
         }
     }
 
-    private void createDigitToLetterMap() {
+    public List<String> letterCombinations(String digits) {
         map = new HashMap<>();
+        result = new ArrayList<>();
+
         map.put('2', new Character[]{'a', 'b', 'c'});
         map.put('3', new Character[]{'d', 'e', 'f'});
         map.put('4', new Character[]{'g', 'h', 'i'});
@@ -49,12 +53,8 @@ class Solution {
         map.put('7', new Character[]{'p', 'q', 'r', 's'});
         map.put('8', new Character[]{'t', 'u', 'v'});
         map.put('9', new Character[]{'w', 'x', 'y', 'z'});
-    }
 
-    public List<String> letterCombinations(String digits) {
-        result = new ArrayList<>();
-
-        if (digits.length() != 0) backtrack(digits, "", 0);
+        if (digits.length() != 0) backtrack(digits, new StringBuilder(), 0);
         return result;
     }
 }
