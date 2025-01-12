40a0f9b05f7537d0b0ed701591179798ef3ce9bb
Rajat16353 <guptarajat3199@gmail.com>
2025-01-12 20:31:23 +0530

    Modified: leet-code/1-50/13-longest palindromic substring.java

--- Leet-Code/1-50/13-Longest Palindromic Substring.java
+++ Leet-Code/1-50/13-Longest Palindromic Substring.java
@@ -102,3 +102,40 @@ class Solution {
         return s.substring(pl, pr + 1);
     }
 }
+
+// Tabulation 2D dp
+class Solution {
+    public String longestPalindrome(String s) {
+        int n = s.length();
+        boolean[][] dp = new boolean[n][n];
+        int longest = -1;
+        int start = -1;
+        int end = -1;
+
+        for (int g = 0; g < n; g++) {
+            for (int i = 0, j = g; j < n; i++, j++) {
+                if (g == 0) {
+                    dp[i][j] = true;
+                } else if (g == 1) {
+                    if (s.charAt(i) == s.charAt(j)) {
+                        dp[i][j] = true;
+                    }
+                } else {
+                    if (s.charAt(i) == s.charAt(j) && dp[i + 1][j - 1]) {
+                        dp[i][j] = true;
+                    }
+                }
+
+                if (dp[i][j]) {
+                    if (longest < j - i + 1) {
+                        longest = j - i + 1;
+                        start = i;
+                        end = j + 1;
+                    }
+                }
+            }
+        }
+
+        return s.substring(start, end);
+    }
+}
